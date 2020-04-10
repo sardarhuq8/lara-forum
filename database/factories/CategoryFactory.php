0dfba2b9d2ad7469af7495f757f784874508e48b
sardar <sardarqsm8@gmail.com>
2020-04-10 19:48:32 +0300

    Factory and seed

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,12 +1,12 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-
-use App\Model\Category;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
-$factory->define(Category::class, function (Faker $faker) {
+$factory->define(App\Model\Category::class, function (Faker $faker) {
+    $word = $faker->word;
     return [
-        //
+        'name' => $word,
+        'slug' => Str::slug($word)
     ];
 });
--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -1,12 +1,21 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
-
-use App\Model\Question;
 use Faker\Generator as Faker;
+use App\Model\Category;
+use Illuminate\Support\Str;
 
-$factory->define(Question::class, function (Faker $faker) {
+$factory->define(App\Model\Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title' => $title,
+        'slug' => Str::slug($title),
+        'body' => $faker->text,
+        'category_id' => function () {
+            return Category::all()->random();
+        },
+        'user_id' => function () {
+            return \App\User::all()->random();
+        }
+        
     ];
 });
