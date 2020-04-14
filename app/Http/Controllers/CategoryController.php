468521e0137c0ebf03dcbf58699cb85a54723fd3
sardar <sardarqsm8@gmail.com>
2020-04-14 20:41:16 +0300

    CRUD for Reply

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,6 +6,7 @@ use App\Model\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
+use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
@@ -16,7 +17,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return category::latest()->get();
+        return CategoryResource::collection(category::latest()->get());
     }
 
     /**
@@ -42,7 +43,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        return $category;
+        return new CategoryResource($category);
     }
 
     /**
